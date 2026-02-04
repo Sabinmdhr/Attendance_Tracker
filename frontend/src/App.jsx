297137d07374c46b28c98cba85e21ec9067b9347
Sabinmdhr <Sabinmanandhar22@gmail.com>
2026-02-04 10:15:08 +0545

    merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/user/Dashboard";
 import AdminDashboard from "./pages/admin/Dashboard";
+import Leaves from "./pages/user/Leaves";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leaves" element={<Leaves />} />
         <Route path="/adminDashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/attendance/AttendanceCalendar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Calendar } from "@/components/ui/calendar";
+const AttendanceCalendar = () => {
+const [date, setDate] = React.useState(new Date());
+const presentDates = [
+  new Date(2026, 1, 3), // Feb 3
+  new Date(2026, 1, 5), // Feb 5
+  new Date(2026, 1, 10), // Feb 10
+];
+  return (
+    <div>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="h-full w-full rounded-lg border border-gray-300 shadow-md p-4"
+        modifiers={{
+          present: presentDates, // Dates when student is present
+        }}
+        modifiersClassNames={{
+          present: "bg-green-500 text-white rounded-full",
+        }}
+      />
+    </div>
+  );
+}
+
+export default AttendanceCalendar
